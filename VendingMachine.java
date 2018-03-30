c8226c26880c13e8f57de503add84c2b3d453148
James <21349877+jpwilliams000@users.noreply.github.com>
2018-03-30 13:43:37 -0400

    project-5-additions-033018
    includes required functionalities as described by Readme SRS

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,5 +1,9 @@
 import java.util.*;
 import java.io.*; // Input / Output Package
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author      James Pata <jpata000#email.cpcc.edu>
@@ -12,7 +16,7 @@ public class VendingMachine
    //private global fields
    private static Scanner keyboard = new Scanner(System.in);
    private static ArrayList<VendingItem> items = new ArrayList<>();
-   private static final int MAX_ITEMS = 10;
+   private static final int MAX_ITEMS = 20;
    
    /**
    *
@@ -23,21 +27,167 @@ public class VendingMachine
    */
    public static void main(String[] args)
    {
+      printTitle();
       loadItemsFromFile();
-      //Controller Logic
+      // 1st menu controller
       boolean goAgain = true; 
       do{ //prompt at least once
          switch(promptHomeMenuChoice()) {//Returned user choice is the argument of the switch 
-            case 1: inputItemsManually();
+            case 1: inputItemsController();
                     break;
-            case 2: purchaseMenu();
+            case 2: removeItemsMenu();
                     break;
-            case 3: goAgain = endProgram(); 
+            case 3: purchaseMenu();
+                    break;
+            case 4: goAgain = endProgram(); 
                     break;
          }
-      } while(goAgain == true);//continue if goAgain is true
+      } while(goAgain); //when true
    }
    
+   // remove items menu print screen
+   public static int promptRemoveItemsMenu()
+   {
+      String userInput;
+      do{
+         System.out.print("\nSelect from one of the following Choices:" +
+                            "\n\t 1 - Remove an Existing Vending item from the Vending Machine" +
+                            "\n\t 2 - Remove all Existing Vending Items from the Vending Machine" +
+                            "\n\t 3 - Return to Main Menu" +
+                            "\n\t Enter your choice (1-3): " );
+        userInput = keyboard.nextLine();
+      }while(!userInput.matches("[1-3]"));//Only number range from 1 to 3
+      return Integer.parseInt(userInput);
+   }
+   
+   // remove items menu controller
+   public static void removeItemsMenu()
+   {
+      boolean goAgain = true;
+      do{
+         switch(promptRemoveItemsMenu()){
+            case 1: removeItem();
+                    goAgain = false;
+                    break;
+            case 2: emptyVendingMachine();
+                    goAgain = false; 
+                    break;
+            case 3: goAgain = false; //return to main 'home' menu
+                    break;
+         }
+      }while(goAgain);
+   }
+   
+   //remove items menu option 1
+   public static void removeItem()
+   {
+      printMachineContents();
+      processRemoveItem(promptItemChoice());
+   }
+   
+   public static void processRemoveItem(int choice)
+   {
+      int index = (choice - 1); 
+      items.remove(index);
+      System.out.println("\n" + items.get(index).getItemName() + " has been removed from the vending machine.");
+   }
+   
+   // remove items menu option 2
+   public static void emptyVendingMachine()
+   {
+      items = new ArrayList<>(); //re-instantiate arraylist, let garbage collector trash old list.
+   }
+   
+   
+   // input items menu print screen
+   public static int promptInputItemMenuChoice()
+   {
+      String userInput;
+      do{
+         System.out.print("\nSelect from one of the following Choices:" +
+                            "\n\t 1 - Re-Stock an Existing Vending Item" +
+                            "\n\t 2 - Re-Stock all Exisitng Vending Items" +
+                            "\n\t 3 - Add a new vending item to the machine" +
+                            "\n\t 4 - Load vending items from a file" +
+                            "\n\t 5 - Return to the Main Menu" +
+                            "\n\t Enter your choice (1-5): " );
+        userInput = keyboard.nextLine();
+      }while(!userInput.matches("[1-5]"));//Only number range from 1 to 5
+      return Integer.parseInt(userInput);
+   }
+   
+   // input items menu controller
+   public static void inputItemsController()
+   {
+      boolean goAgain = true;
+      do{
+         switch(promptInputItemMenuChoice()){
+            case 1: restockItem();
+                    goAgain = false;
+                    break;
+            case 2: reStockEntireVendingMachine();
+                    goAgain = false; 
+                    break;
+            case 3: inputItemsManually();
+                    goAgain = false;
+                    break;
+            case 4: loadItemsFromFile();
+                    goAgain = false;
+            case 5: goAgain = false; //return to home menu
+                    break;
+         }
+      }while(goAgain);
+   }
+   
+   // input items menu option 1
+   public static void restockItem()
+   {
+      printMachineContents();
+      processRestockItem(promptItemChoice());
+   }
+   
+   public static void processRestockItem(int choice)
+   {
+      int index = (choice - 1); 
+      items.get(index).restock();
+   }
+   
+   public static int promptItemChoice()
+   {
+      int upperBound = items.size();
+      String choice;
+      do{
+         System.out.print("Enter your choice: (1-"+upperBound+"): ");
+         choice = keyboard.nextLine();
+      }while(!choice.matches("^[1-9]\\d*\\.?[0]*$") //whole numbers only
+            || Integer.parseInt(choice) > upperBound ); //nothing greater than number of items
+     
+     return Integer.parseInt(choice);
+   }
+
+   
+   // input items menu option 2
+   public static void reStockEntireVendingMachine()
+   {
+      for(VendingItem i : items) i.restock();
+   }
+      
+   /**
+   *
+   *
+   *  printTitle() will print the application information to the console.
+   *
+   *
+   */
+   private static void printTitle()
+   {  
+      System.out.println("*****************************");
+      System.out.println("*VENDING MACHINE PROJECT 5  *");
+      System.out.println("* \tAuthor: Mr. Just James   *");
+      System.out.println("* \tProject 2 Date 02/19/18  *");
+      System.out.println("* \tProject 5 Date 03/30/18  *");
+      System.out.println("*****************************");
+   }
       
    /**
    * 
@@ -57,11 +207,12 @@ public class VendingMachine
       do{
          System.out.print("\nChoose from one of the following Menu Choices:" +
                             "\n\t 1 - Manually Enter Item(s) into the Vending Machine" +
-                            "\n\t 2 - Purchase something from the Vending Machine" +
-                            "\n\t 3 - Exit the program" +
+                            "\n\t 2 - Manually Remove Item(s) from the Vending Machine" +
+                            "\n\t 3 - Purchase something from the Vending Machine" +
+                            "\n\t 4 - Exit the program" +
                             "\n\t Enter your choice (1,2, or 3): " );
         userInput = keyboard.nextLine();
-      }while(!userInput.matches("[1-3]"));//Only number range from 1 to 3 
+      }while(!userInput.matches("[1-4]"));//Only number range from 1 to 4
       return Integer.parseInt(userInput);
    }
    
@@ -97,7 +248,7 @@ public class VendingMachine
   *
   *
   * Choice # 1's - Prompt user for information about items to be input manually.
-  *                Only runs if isSpace is true.
+  *                **Only runs if isSpace is true.
   *
   *
   */
@@ -107,10 +258,15 @@ public class VendingMachine
          
          while (repeatCounter < numberOfItems) //Keep requesting product info until number to be added is met
          {   
+            int itemTypeValue = promptItemType();
             String itemName = promptItemName();
             double itemCost = promptItemCost(itemName);
-            int itemQuantity = promptItemQuantity(itemName);
-            VendingItem itemInfo = new VendingItem(itemName, itemCost, itemQuantity);
+            int itemQuantity = promptItemQuantity(itemName, itemTypeValue);
+            //TODO DRY from file reading method (can be enhanced)such as 'items.add(insertNewItem())'
+            VendingItem itemInfo = new VendingItem();
+            if(itemTypeValue == 1) itemInfo = new Snack(itemName, itemCost, itemQuantity);
+            if(itemTypeValue == 2) itemInfo = new CannedBeverage(itemName, itemCost, itemQuantity);
+            if(itemTypeValue == 3) itemInfo = new BottledBeverage(itemName, itemCost, itemQuantity);
             items.add(itemInfo);
             repeatCounter++;   
          }
@@ -173,6 +329,26 @@ public class VendingMachine
       return unique;
    }
    
+   /**
+  * 
+  * 
+  * Choice # 1's - Prompt user for item encoded key value's type.
+  *
+  * 
+  */
+   public static int promptItemType()
+   {
+      String itemType;
+      do{
+         System.out.print("Choose from one of the following types of vending items to be added to the machine: "
+                          + "\n\t 1 - Snack \n\t 2 - Canned Beverage \n\t 3 - Bottled Beverage"
+                          + "\n\t Enter your choice (1, 2, or 3):");
+         itemType = keyboard.nextLine();
+      } while (!itemType.matches("[1-3]")); //numbers 1-3 only 
+      return Integer.parseInt(itemType);
+   }
+   
+  
   /**
   * 
   * 
@@ -201,14 +377,24 @@ public class VendingMachine
   *
   * 
   */
-   public static int promptItemQuantity(String itemName)
+   public static int promptItemQuantity(String itemName, int itemType)
    {
       String itemQuantity;
+      int maxInputQuantity = determineMaxInputQuantity(itemType);
       do{
-         System.out.print("Enter the quantity of " + itemName + " being loaded into the machine: (Max: 10) ");
+         System.out.print("Enter the quantity of " + itemName + " being loaded into the machine: (Max: " + maxInputQuantity + ") ");
          itemQuantity = keyboard.nextLine();
-      } while (!itemQuantity.matches("^[1-9]\\d*\\.?[0]*$") || Integer.parseInt(itemQuantity) > 10);//Natural Numbers
+      } while (!itemQuantity.matches("^[1-9]\\d*\\.?[0]*$") || Integer.parseInt(itemQuantity) > maxInputQuantity);//Natural Numbers
       return Integer.parseInt(itemQuantity);
+   }
+   
+   public static int determineMaxInputQuantity(int itemType)
+   {
+      int determineMaxInputQuantity = 0;
+      if(itemType == 1) determineMaxInputQuantity = Snack.getMaxCapacity();
+      if(itemType == 2) determineMaxInputQuantity = CannedBeverage.getMaxCapacity();
+      if(itemType == 3) determineMaxInputQuantity = BottledBeverage.getMaxCapacity();
+      return determineMaxInputQuantity;
    }
    
   /**
@@ -349,7 +535,7 @@ public class VendingMachine
       if(items.size()>0) items = new ArrayList<>(); 
       
       try {
-         File file = new File("ProductList.txt");
+         File file = new File("ProductListNew.txt");
          Scanner input = new Scanner(file);
          while (input.hasNext()) 
          {
@@ -359,10 +545,15 @@ public class VendingMachine
                Double itemCost = Double.parseDouble(itemCostString);
             String itemQuantityString = input.nextLine();
                int itemQuantity = Integer.parseInt(itemQuantityString);
+            String itemTypeString = input.nextLine();
+               int itemType = Integer.parseInt(itemTypeString);   
             //If blank line, then consume it               
             if(input.hasNext())input.nextLine();
-            //create a VendingItem       
-            VendingItem itemInfo = new VendingItem(itemName, itemCost, itemQuantity);
+            //**TODO DRY similar code in manual inserting method***
+            VendingItem itemInfo = new VendingItem();
+            if(itemType == 1) itemInfo = new Snack(itemName, itemCost, itemQuantity);
+            if(itemType == 2) itemInfo = new CannedBeverage(itemName, itemCost, itemQuantity);
+            if(itemType == 3) itemInfo = new BottledBeverage(itemName, itemCost, itemQuantity);
             //add VendingItem to global arraylist
             items.add(itemInfo);
          }
